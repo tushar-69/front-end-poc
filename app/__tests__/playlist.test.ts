e53ff1563a393706fde47833085a85f13208c61e
Tushar Singh <tusharmritunjay.singh@cesltd.com>
2023-12-18 18:47:30 +0530

    fixed test cases

--- app/__tests__/playlist.test.ts
+++ app/__tests__/playlist.test.ts
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import IPlayList from '../playlist/iPlaylist';
 import { ReactElement } from 'react';
 import { faker } from "@faker-js/faker";
-import PlayListPage  from '../playlist/page'
+import PlayList  from '../playlist/playlist'
 import React from 'react';
 
 jest.mock("../playlist/page", () => 
@@ -12,6 +12,7 @@ jest.mock("../playlist/page", () =>
 
 describe('PlayList', () => {
     var playListMock: jest.Mock<any, any, any>;
+    var updatePlayListDataMock: jest.Mock<any, any, any>;
     var toggleAddPlayListMock: jest.Mock<any, any, any>;
     var toggleUpdatePlayListMock: jest.Mock<any, any, any>;
 
@@ -23,64 +24,73 @@ describe('PlayList', () => {
         playListMock = jest.fn();
         toggleAddPlayListMock = jest.fn();
         toggleUpdatePlayListMock = jest.fn();
-        React.useContext = jest.fn().mockImplementation(() => playListMock);
-        React.useContext = jest.fn().mockImplementation(() => toggleAddPlayListMock);
-        React.useContext = jest.fn().mockImplementation(() => toggleUpdatePlayListMock);
+        React.useState = jest.fn().mockImplementation(() => playListMock);
+        React.useState = jest.fn().mockImplementation(() => updatePlayListDataMock);
+        React.useState = jest.fn().mockImplementation(() => toggleAddPlayListMock);
+        React.useState = jest.fn().mockImplementation(() => toggleUpdatePlayListMock);
       }
 
-    it("On initial render with no playlist page", async () => {
-        const component = PlayListPage();
-        const { container } = render(component as ReactElement);
-        expect(container.querySelector("h2")?.innerHTML).toBe("there are no playlist in the Database");
+    describe('empty playlist', () => {
+        it("it should not have playlists", async () => {
+            const component = PlayList();
+            const { container } = render(component as ReactElement);
+            expect(container.querySelector("h2")?.innerHTML).toBe("there are no playlist in the Database");
+        })
     })
 
-    it("On initial render with a playlist available in the database", async () => {
+    describe('playlist with value', () => {
         const playList: IPlayList = { 
             name: faker.string.alphanumeric(), 
             movies: [faker.string.alphanumeric()]
         };
-        setup(playList);
-        expect(playListMock).toHaveBeenCalledWith(playList); 
 
-        const component = PlayListPage();
-        render(component as ReactElement);
-        const titleElement = await screen.findByTestId("title");
-        expect(titleElement).toBeInTheDocument();
-        expect(titleElement.innerHTML).toBe("PlayList");
+        beforeAll(() => {
+            setup(playList);
+        })
 
-        const tableElement = await screen.getByRole('table');
-        expect(tableElement).toBeInTheDocument();
-
-        const buttonElement = await screen.findAllByRole('button');
-        expect(buttonElement).toBeInTheDocument();
-        expect(buttonElement).toEqual(3);
+        it("should have playlists", async () => {
+            expect(playListMock).toHaveBeenCalledWith(playList); 
+    
+            const component = PlayList();
+            render(component as ReactElement);
+            const titleElement = await screen.findByTestId("title");
+            expect(titleElement).toBeInTheDocument();
+            expect(titleElement.innerHTML).toBe("PlayList");
+    
+            const tableElement = await screen.getByRole('table');
+            expect(tableElement).toBeInTheDocument();
+    
+            const buttonElement = await screen.findAllByRole('button');
+            expect(buttonElement).toBeInTheDocument();
+            expect(buttonElement).toEqual(3);
+        })
+    
+        it("On addplaylist a new playlist is added", async () => {
+            const component = PlayList();
+            render(component as ReactElement);
+            
+            const btnAddElement = await screen.findByTestId("btnAddPlayList");
+            expect(btnAddElement).toBeInTheDocument();
+    
+            expect(toggleAddPlayListMock).toHaveBeenCalledWith(true);
+        })
+    
+        it("On removeplaylist a playlist is deleted", async () => {
+            const component = PlayList();
+            render(component as ReactElement);
+            const btnDeleteElement = await screen.findByTestId("btnDeletePlayList");
+            expect(btnDeleteElement).toBeInTheDocument();
+        })
+    
+        it("On updateplaylist a playlist is updated", async () => {
+            const component = PlayList();
+            render(component as ReactElement);
+            const btnDeleteElement = await screen.findByTestId("btnUpdatePlayList");
+            expect(btnDeleteElement).toBeInTheDocument();
+    
+            expect(toggleUpdatePlayListMock).toHaveBeenCalledWith(true);
+        })
     })
 
-    it("On addplaylist a new playlist is added", async () => {
-        const playList: IPlayList = { name: faker.string.alphanumeric(), movies: [faker.string.alphanumeric()]};
-        const component = PlayListPage();
-        render(component as ReactElement);
-        
-        const btnAddElement = await screen.findByTestId("btnAddPlayList");
-        expect(btnAddElement).toBeInTheDocument();
-
-        expect(toggleAddPlayListMock).toHaveBeenCalledWith(true);
-    })
-
-    it("On removeplaylist a playlist is deleted", async () => {
-        const component = PlayListPage();
-        render(component as ReactElement);
-        const btnDeleteElement = await screen.findByTestId("btnDeletePlayList");
-        expect(btnDeleteElement).toBeInTheDocument();
-    })
-
-    it("On updateplaylist a playlist is updated", async () => {
-        const playList: IPlayList = { name: faker.string.alphanumeric(), movies: [faker.string.alphanumeric()]};
-        const component = PlayListPage();
-        render(component as ReactElement);
-        const btnDeleteElement = await screen.findByTestId("btnUpdatePlayList");
-        expect(btnDeleteElement).toBeInTheDocument();
-
-        expect(toggleUpdatePlayListMock).toHaveBeenCalledWith(true);
-    })
+    
 })
